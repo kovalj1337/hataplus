eb5e98af48cc9507530c6a8063344bf4b5ae514a
kovalj1337 <vanaplayers@gmail.com>
2024-09-06 21:08:21 +0400

    dd

--- script.js
+++ script.js
@@ -6,10 +6,18 @@ cancel.onclick = () => {
     popup.style.display="none"
     black.style.display="none"
 }
-const burger = document.querySelector(".burger")
-      menu = document.querySelector(".text-navigation")
-      cancel = document.querySelector(".cancel-burger")
+const burger = document.querySelector(".burger"),
+      menu = document.querySelector(".text-navigation"),
+      cancelBurger = document.querySelector(".cancel-burger")
 
       burger.addEventListener("click",function(){
         menu.style.left = "0"
+        black.style.display="block"
+        console.log("+")
       })
+      cancelBurger.addEventListener("click",function(){
+        menu.style.left = "-100%"
+        console.log("+")
+        black.style.display="none"
+      })
+      
